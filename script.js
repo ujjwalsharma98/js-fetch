9dce48f234858f9e9ba4081276ac4e800211dbb7
Vishal <cs_dev1@kitabeli.id>
2020-04-21 17:29:22 +0530

    xhr request

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const list = document.getElementById('ul-list')
 const imgTag = document.getElementById('img-tag')
+const dogImage = document.getElementById('dog-image')
 
 let fetchWork = function () {
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => response.json())
         .then(json => {
             for (let i = 0; i < json.length; i++) {
-                console.log(i, ': ', json[i].title)
                 let a = document.createElement('li')
                 a.innerHTML = json[i].title;
                 list.appendChild(a)
@@ -14,13 +14,24 @@ let fetchWork = function () {
         })
 }
 
-let xhrWork = function () {
+let fetchDog = function () {
     fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(json => {
-        imgTag.setAttribute('src',json.message)
-    })
+        .then(response => response.json())
+        .then(json => {
+            imgTag.setAttribute('src', json.message)
+        })
+}
+
+let xhrDog = function () {
+    var xhrRequest = new XMLHttpRequest()
+    xhrRequest.onload = function(){
+        var parsedData = JSON.parse(xhrRequest.response)
+        dogImage.setAttribute('src', parsedData.message)
+    }
+    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random', true)
+    xhrRequest.send()
 }
 
 document.getElementById("fetchButton").addEventListener("click", fetchWork);
-document.getElementById("xhrButton").addEventListener("click", xhrWork);
+document.getElementById("dogFetch").addEventListener("click", fetchDog);
+document.getElementById("xhrButton").addEventListener("click", xhrDog);
